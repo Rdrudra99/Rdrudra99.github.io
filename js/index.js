072a022b23139854777b2b230e7af0d93a560ae5
Rudra Narayan Boitei <rudra2021@gift.edu.in>
2022-06-11 10:48:55 +0530

    push

--- js/index.js
+++ js/index.js
@@ -3,8 +3,10 @@ icon.onclick = function () {
   document.body.classList.toggle("darktheme");
   if (document.body.classList.contains("darktheme")) {
     icon.src = "images/sun.png"
+    profile.src = "images/red1.jpg"
   }
   else {
     icon.src = "images/moon.png"
+    profile.src = "images/blue1.jpg"
   }
 }
