2d3dd2718303e078bb449749ddfa754cfdab3817
Rudra Narayan Boitei <rudra2021@gift.edu.in>
2022-07-05 09:51:24 +0530

    expand Website

--- js/index.js
+++ js/index.js
@@ -4,12 +4,15 @@ icon.onclick = function () {
   if (document.body.classList.contains("darktheme")) {
     icon.src = "images/sun.png"
     profile.src = "images/red1.jpg"
-    music.src = "audio/rudra.mp3"
+    // music.src = "audio/rudra.mp3"
+    skill.src = "images/program2.png"
+    list.src = "../images/college_2.svg"
   }
   else {
     icon.src = "images/moon.png"
     profile.src = "images/blue1.jpg"
-    music.src = ""
+    skill.src = "images/program1.png"
+    list.src = "../images/college_1.svg"
 
   }
 }
