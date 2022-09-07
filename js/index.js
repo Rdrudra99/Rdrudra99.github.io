bc48c0c99d8f23d2ea86a6cd23e85ffc22c568a4
Rudra Narayan Boitei <rudra2021@gift.edu.in>
2022-09-07 22:31:27 +0530

    Profile Picture Change🚀

--- js/index.js
+++ js/index.js
@@ -3,11 +3,11 @@ icon.onclick = function () {
   document.body.classList.toggle("darktheme");
   if (document.body.classList.contains("darktheme")) {
     icon.src = "images/sun.png"
-    profile.src = "images/rudra (1).jpg"
+    // profile.src = "images/rudra (1).jpg"
     // music.src = "audio/rudra.mp3"
   }
   else {
     icon.src = "images/moon.png"
-    profile.src = "images/Untitled (1080 × 1080 px).png"
+    // profile.src = "images/Untitled (1080 × 1080 px).png"
   }
 }
